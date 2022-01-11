89584ef611b4d39dd02b3583ebd4d97686d4d8a8
kanika637 <kanikagola26@gmail.com>
2022-01-11 21:53:36 +0530

    Timer added, project completed

--- Projects/QuizApp/script.js
+++ Projects/QuizApp/script.js
@@ -131,6 +131,29 @@ class Quiz{
 
     displayQuestion();
 
+    //ADD COUNTDOWN
+
+    let time=10;
+    let quizTimeMinutes=time*60*60;
+    quizTime=quizTimeMinutes/60;
+
+    let counting=document.getElementById("count-down")
+
+    function startCountDown(){
+        let quizTimer=setInterval(function(){
+            if(quizTime<=0){
+                clearInterval(quizTimer);
+                showScores();
+            }else{
+                quizTime--;
+                let sec=Math.floor(quizTime%60);
+                let min=Math.floor(quizTime/60)%60;
+                counting.innerHTML=`TIME: ${min} : ${sec}`;
+            }
+        },1000)
+    }
+
+    startCountDown();
 
 
 
